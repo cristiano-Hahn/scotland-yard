4ce23315dcc3248b3b2e1e79f75ffd81d1e6e942
Cristiano Hahn <cristianohahn.ch@gmail.com>
2023-07-18 20:17:05 -0300

    added main screen

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
-import { Button } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import scotlandYardTips from '../../assets/scotland_yard_tips.json'
+
+const Title = styled(Box)(({ theme }) => ({
+    ...theme.typography.h1,
+    margin: theme.spacing(4),
+    textAlign: 'center'
+}));
+
+const Book = styled(Box)(({ theme }) => ({
+    ...theme.typography.h4,
+    margin: theme.spacing(4),
+    color: theme.palette.text.primary,
+}));
+
+const CaseButton = styled(Button)(({ theme }) => ({
+    ...theme.typography.body1,
+    margin: theme.spacing(1),
+    color: theme.palette.text.secondary,
+    width: '100%',
+    maxWidth: '250px',
+    marginLeft: '20px',
+    marginRight: '20px'
+}));
+
+const MainScreen = styled(Box)(({ theme }) => ({
+    maxWidth: '700px',
+    margin: 'auto'
+}));
+
 
 function Home() {
     return (
-        <Button variant='contained'>HOME</Button>
+        <>
+            <MainScreen sx={{ flexGrow: 1 }}>
+                <Grid container>
+                    <Grid xs={12}>
+                        <Title>Scotland Yard</Title>
+                    </Grid>
+                    <Grid xs={12}>
+                        {scotlandYardTips.books.map((book) =>
+                            <Grid container >
+                                <Grid xs={12}>
+                                    <Book>{book.name}</Book>
+                                </Grid>
+
+                                {book.cases.map((caseNumber) =>
+                                    <Grid xs={6} style={{display: 'flex', justifyContent: 'center'}}>
+                                        <CaseButton variant='contained'>{caseNumber.name}</CaseButton>
+                                    </Grid>
+                                )}
+                            </Grid>
+                        )}
+                    </Grid>
+                </Grid>
+            </MainScreen>
+        </>
     );
 }
 
--- src/theme/themeOptions.js
+++ src/theme/themeOptions.js
@@ -2,7 +2,7 @@ export default  {
   palette: {
     mode: 'dark',
     primary: {
-      main: '#9c8012',
+      main: '#3d3222',
       contrastText: '#ffffff',
     },
     secondary: {
@@ -10,12 +10,34 @@ export default  {
     },
   },
   shape: {
-    borderRadius: 24,
+    borderRadius: 8,
   },
   typography: {
     fontWeightMedium: 500,
-    button: {
-      fontWeight: 500,
+    h1: {
+      fontFamily: 'Dancing Script',
+      fontSize: '3rem',
+      color: '#FFB038'
+    },
+    fontFamily: 'Cabin Condensed',
+    h2: {
+      fontFamily: 'Dancing Script',
+      fontSize: '2.5rem',
+    },
+    h3: {
+      fontFamily: 'Dancing Script',
+      fontSize: '2rem',
+    },
+    h4: {
+      fontFamily: 'Dancing Script',
+      fontSize: '1.7rem',
+    },
+    h5: {
+      fontFamily: 'Dancing Script',
+      fontSize: '1.4rem',
+    },
+    h6: {
+      fontFamily: 'Dancing Script',
     },
   },
 };
